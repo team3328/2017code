652dd181938bc0ed449dfa04603c1acf70c2c2fc
Malcolm <Malcolm@10.241.34.150>
2017-01-20 09:46:11 -0800

    NetworkTables robot side and client side framework

---
+++ SensorRobot/src/org/usfirst/frc/team3328/robot/Comms.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team3328.robot;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class Comms {
+
+	NetworkTable table;
+	
+	public Comms(){
+		table = NetworkTable.getTable("numTable");
+	}
+	
+	/*public void update(){
+		table.putNumber(", value)
+	}
+	*/
+	
+}
---
+++ SensorRobot/src/org/usfirst/frc/team3328/robot/NetworkTablesClientSide.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team3328.robot;
+
+public class NetworkTablesClientSide {
+
+}
