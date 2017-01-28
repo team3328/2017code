80ee8576b0fc9e3dc23f84f9eee420c8c6b8c3e9
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-01-28 10:12:19 -0800

    added networktables thread

--- 2017code/src/org/usfirst/frc/team3328/robot/Robot.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/Robot.java
@@ -5,21 +5,32 @@ import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 
 public class Robot extends IterativeRobot {
-	SpeedController fl = new Talon(0);
-	SpeedController fr = new Talon(1);
-	SpeedController bl = new Talon(2);
-	SpeedController br = new Talon(3);
-	Controller xbox = new SteamWorksController(false);
-	DriveSystem drive = new SteamWorksDriveSystem(fl, fr, bl, br, xbox);
-	Climber climb = new Climber(xbox);
-	ADIS16448_IMU imu = new ADIS16448_IMU();
-	Controller cont = new SteamWorksController(false);
-	Comms comms = new Comms();
-	Target target = new Target();
+	SpeedController fl;
+	SpeedController fr;
+	SpeedController bl;
+	SpeedController br;
+	Controller xbox;
+	DriveSystem drive;
+	Climber climb;
+	ADIS16448_IMU imu;
+	Controller cont;
+	Comms comms;
+	Target target;
 	NetworkTablesTargetProvider targetProvider;
 
 	@Override
 	public void robotInit() {
+		fl = new Talon(0);
+		fr = new Talon(1);
+		bl = new Talon(2);
+		br = new Talon(3);
+		xbox = new SteamWorksController(false);
+		drive = new SteamWorksDriveSystem(fl, fr, bl, br, xbox);
+		climb = new Climber(xbox);
+		imu = new ADIS16448_IMU();
+		cont = new SteamWorksController(false);
+		comms = new Comms();
+		target = new Target();
 		targetProvider = new NetworkTablesTargetProvider();
 		imu.init();
 	}
@@ -36,8 +47,8 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopPeriodic() {
-		drive.controlledMove();
-		drive.printSpeed();
+		/*drive.controlledMove();
+		drive.printSpeed();*/
 		/*comms.update();
 		target.printValues();*/
 		//System.out.println(drive.updateDisplacement(0, imu.getAngleZ()));
