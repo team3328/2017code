0e8c9386cff703142c983385b1ab68249509a610
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-01-27 18:03:15 -0800

    added displacement function

--- 2017code/src/org/usfirst/frc/team3328/robot/DriveSystem.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/DriveSystem.java
@@ -8,10 +8,10 @@ public class DriveSystem {
 	private Talon fl, fr, bl, br;
 	private double restraint = 1;
 	private double factor = 1.1;
+	private double displacement;
 	private boolean active = true;
 	
-	//instantiates talons and controller
-	//sets controller to boolean to choose between joystick(true) or xbox(false)
+	//instantiates talons assigns controller to "con"
 	public DriveSystem(Controller controller){
 		fl = new Talon(0);
 		fr = new Talon(1);
@@ -34,6 +34,7 @@ public class DriveSystem {
 		bl.set(speed);
 	}
 	
+	//stops motors
 	private void stop(){
 		fl.set(0);
 		bl.set(0);
@@ -44,6 +45,8 @@ public class DriveSystem {
 		System.out.printf("%.2f || %.2f\n",fl.get(), fr.get());
 	}
 	
+	
+	//Dynamic updating for strength of angle correction during auto
 	private void updateFactor(){
 		if (con.getButton(1) && factor > 1){
 			factor -= .1;
@@ -65,6 +68,7 @@ public class DriveSystem {
 		}
 	}
 	
+	//toggles drive off and the climb system on because they share controlls
 	private boolean driveActive(){
 		if (con.getButton(1)){
 			active = !active;
@@ -73,15 +77,28 @@ public class DriveSystem {
 		return active;
 	}
 	
+	//gets displacement between the desired angle and the current angle
+	//normalizes displacement so it's between 0 - 1
+	//rounds displacement off to 2 decimal places
+	//all values above 0 and below .05 are set to .05
+	public double updateDisplacement(double desired, double current){
+		displacement = (current - desired) / 360;
+		displacement = Math.round(displacement * 100) / 100;
+		if (displacement > 0 && displacement < .05){
+			displacement = .05;
+		}
+		return displacement;
+	}
+	
 	//Uses the gyro to turn until it reaches a desired angle.
 	//should work while moving and while stopped
-	//the speed of each side is separately adjusted using the displacement after it's been normalized to a value between 0 - 1
+	//the speed of each side is separately adjusted using the displacement
 	public void autoAngle(double speed, double current, double desired){
 		updateFactor();
 		System.out.println(factor);
-		double displacement = (current - desired);
-		right((speed + (displacement / 360)) * factor);
-		left(speed - (displacement / 360) * factor);
+		updateDisplacement(desired, current);
+		right((speed + displacement) * factor);
+		left((speed - displacement) * factor);
 	}
 	
 	public void autoMove(double distance){
