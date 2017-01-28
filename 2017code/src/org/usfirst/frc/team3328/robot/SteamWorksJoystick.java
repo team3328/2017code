aec35e7fcc3b2aca32e6855ee3c070d07b1812e2
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-01-28 10:34:55 -0800

    replaced swcontroller class with seperate xbox and joystick classes

---
+++ 2017code/src/org/usfirst/frc/team3328/robot/SteamWorksJoystick.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team3328.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class SteamWorksJoystick implements Controller {
+	
+	Joystick joy;
+
+	//array to store previous value of buttons
+	boolean[] button = new boolean[10];
+	
+	//instantiates the joystick
+	public SteamWorksJoystick(boolean type){
+		joy = new Joystick(0);
+	}
+	
+	//returns the x axis value for the joystick
+	@Override
+	public double getX(){
+		return joy.getX();
+	}
+	
+	//returns the y axis for the joystick
+	@Override
+	public double getY(){
+		return joy.getY();
+	}
+	
+	//returns true when the button with the index "num" has been released
+	@Override
+	public boolean getButton(int num){
+		if (joy.getRawButton(num) &&  !button[num]){
+			button[num] = joy.getRawButton(num);
+			return true;
+		}
+		button[num] = joy.getRawButton(num);
+		return false;
+	}
+	
+	//return true when button "num" 
+	@Override
+	public boolean getButtonPress(int num){
+		return joy.getRawButton(num);
+	}
+
+	
+}
