cf05a2ded3c94848358f42b654b87554857f33b8
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-01-31 09:45:58 -0800

    removed all joystick code

--- 2017code/src/org/usfirst/frc/team3328/robot/SteamWorksXbox.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/SteamWorksXbox.java
@@ -1,52 +1,27 @@
 package org.usfirst.frc.team3328.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 
-public class SteamWorksController implements Controller {
+public class SteamWorksXbox implements Controller {
 	
-	Joystick joy;
 	XboxController xbox;
-	
-	//true if you're using a joystick
-	boolean joystick;
 	//array to store previous value of buttons
 	boolean[] button = new boolean[10];
 	
-	//instantiates a joystick if true an xbox controller if false.
-	public SteamWorksController(boolean type){
-		if(type){
-			joystick = true;
-			joy = new Joystick(0);
-		}else{
-			joystick = false;
+	//instantiates an xbox controller
+	public SteamWorksXbox(){
 			xbox = new XboxController(0);
-		}
 	}
 	
-	//returns the x axis value for the joystick
-	//returns the right trigger minus the left trigger for the xbox to allow for forwards and reverse
-	/* (non-Javadoc)
-	 * @see org.usfirst.frc.team3328.robot.Controller#getX()
-	 */
+	//returns the right trigger minus the left trigger to allow for forwards and reverse
 	@Override
 	public double getX(){
-		if (joystick){
-			return joy.getX();
-		}
 		return xbox.getRawAxis(3) - xbox.getRawAxis(2);
 	}
 	
-	//returns the y axis for the joystick
-	//returns the x axis for the xbox with a deadband of .1
-	/* (non-Javadoc)
-	 * @see org.usfirst.frc.team3328.robot.Controller#getY()
-	 */
+	//returns the x axis with a deadband of .1
 	@Override
 	public double getY(){
-		if (joystick){
-			return joy.getY();
-		}
 		if (Math.abs(xbox.getRawAxis(0)) > .1){
 			return xbox.getRawAxis(0);
 		}
@@ -54,14 +29,8 @@ public class SteamWorksController implements Controller {
 	}
 	
 	//returns true when the button with the index "num" has been released
-	/* (non-Javadoc)
-	 * @see org.usfirst.frc.team3328.robot.Controller#getButton(int)
-	 */
 	@Override
 	public boolean getButton(int num){
-		if(joystick){
-			return joy.getRawButton(num);
-		}
 		if (xbox.getRawButton(num) &&  !button[num]){
 			button[num] = xbox.getRawButton(num);
 			return true;
@@ -70,18 +39,14 @@ public class SteamWorksController implements Controller {
 		return false;
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.usfirst.frc.team3328.robot.Controller#getButtonPress(int)
-	 */
+	//returns true while the button is pressed not when released
 	@Override
 	public boolean getButtonPress(int num){
 		return xbox.getRawButton(num);
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.usfirst.frc.team3328.robot.Controller#getPad()
-	 */
-	@Override
+	//returns Dpad values
+	//0 up 180 down clockwise increasing
 	public double getPad(){
 		return xbox.getPOV();
 	}
