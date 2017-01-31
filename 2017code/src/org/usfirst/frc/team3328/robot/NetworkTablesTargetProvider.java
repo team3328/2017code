397795beb28c731a491d85c2dbd38d5c3550a05a
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-01-31 09:17:18 -0800

    starts "listenerThread" and encapsulates target object

---
+++ 2017code/src/org/usfirst/frc/team3328/robot/NetworkTablesTargetProvider.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team3328.robot;
+
+public class NetworkTablesTargetProvider implements TargetProvider{
+	
+	Target target = new Target();
+	
+	//Starts "listenerThread" upon instantiation
+	Listener listener = new Listener("listenerThread", target);
+	
+	@Override
+	public Target getTarget() {
+		return target;
+	}
+
+	
+	
+}
