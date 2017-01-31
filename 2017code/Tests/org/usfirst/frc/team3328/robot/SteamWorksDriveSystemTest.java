80b59f1dfe4954a86781873c96b1f037daead500
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-01-31 09:18:26 -0800

    Added test for updateDisplacement

--- 2017code/Tests/org/usfirst/frc/team3328/robot/SteamWorksDriveSystemTest.java
+++ 2017code/Tests/org/usfirst/frc/team3328/robot/SteamWorksDriveSystemTest.java
@@ -7,22 +7,32 @@ import org.junit.Test;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class SteamWorksDriveSystemTest {
+	
+	FakeController fakeCont = new FakeController();
+	FakeSpeedController fl = new FakeSpeedController();
+	FakeSpeedController fr = new FakeSpeedController();
+	FakeSpeedController bl = new FakeSpeedController();
+	FakeSpeedController br = new FakeSpeedController();
+	FakeADIS16448_IMU imu = new FakeADIS16448_IMU();
+	SteamWorksDriveSystem drive = new SteamWorksDriveSystem(fl, fr, bl, br, fakeCont);
 
 	@Test
 	public void controlledMove_yLargerThanX_rightMotorTurnsBackwards() {
-		FakeController fakeCont = new FakeController();
 		fakeCont.setY(1);
 		fakeCont.setX(0);
-		FakeSpeedController fl = new FakeSpeedController();
-		FakeSpeedController fr = new FakeSpeedController();
-		FakeSpeedController bl = new FakeSpeedController();
-		FakeSpeedController br = new FakeSpeedController();
-		SteamWorksDriveSystem drive = new SteamWorksDriveSystem(fl, fr, bl, br, fakeCont);
 		drive.controlledMove();
 		assertEquals(1.0, (fr.speed),  0);
 	}
 	
+	@Test
+	public void updateDisplacement_smallDisplacement_displacementSetToPoint5(){
+		imu.setAngleZ(Math.random() / 20);
+		assertEquals(drive.updateDisplacement(0, imu.getAngleZ()), .05, 0);
+	}
+	
+	
+	
 }
 	
-	//Function Arrange Act Assert
+	// Arrange Act Assert
 	
