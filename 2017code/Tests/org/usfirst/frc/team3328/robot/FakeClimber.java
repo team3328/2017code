77eb857947798d48e9783f44b37adaca0ac88d8f
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-02 16:35:37 -0800

    created fake climber class for testing

---
+++ 2017code/Tests/org/usfirst/frc/team3328/robot/FakeClimber.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team3328.robot;
+
+public class FakeClimber implements Climber {
+
+	@Override
+	public void driverControl() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public double setNum(double num){
+		return num;
+	}
+
+}
