a24d71785f63cd08324e05ded0a86dfff9c366a9
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-03 18:14:27 -0800

    Created a Hotel Lobby class

---
+++ 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksHotelLobby.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team3328.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class SteamWorksHotelLobby implements HotelLobby {
+
+	public SteamWorksHotelLobby(SpeedController talonController) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void controlBelt() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
