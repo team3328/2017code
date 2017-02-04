44c0b3786cb5ecef505565c76fb06242b3b8cf3a
Malcolm Canterbury <canterburymalcolm@gmail.com>
2017-02-03 18:08:09 -0800

    This one's the real shooter class

--- 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksShooter.java
+++ 2017code/src/org/usfirst/frc/team3328/robot/subsystems/SteamWorksShooter.java
@@ -11,26 +11,34 @@ public class SteamWorksShooter implements Shooter {
 	SpeedController t1;
 	SpeedController t2;
 	Controller con;
+	private double speed = 0;
 	boolean active = false;
 	
-	public SteamWorksShooter(Encoder encoder, SpeedController talonController1, SpeedController talonController2, Controller controller){
+	public SteamWorksShooter(Encoder encoder, SpeedController talonController1,
+							 SpeedController talonController2, Controller controller){
 		coder = encoder;
 		t1 = talonController1;
 		t2 = talonController2;
-		con = controller; 
+		con = controller;
 	}
 	
 	@Override
-	public double getSpeed(){
-		return coder.getRate();
+	public boolean isMax(){
+		return coder.get() >= 1000;
 	}
 	
+	// Gradually builds up speed to max 
 	private void maxSpeed(){
-		t1.set(1);
-		t2.set(2);
+		if (speed < 1){
+			speed += 0.02;
+		}
+		t1.set(speed);
+		t2.set(speed);
 	}
-
+	
+	// Sets talons and speed to 0
 	private void stop(){
+		speed = 0;
 		t1.set(0);
 		t2.set(0);
 	}
@@ -40,14 +48,15 @@ public class SteamWorksShooter implements Shooter {
 	// max speed whether or not.
 	@Override
 	public void shooterControl(){
-		if (con.getButtonRelease(3)){
-			if (active){
-				stop();
-			}else{
-				maxSpeed();
-			}
+		if (con.getButtonRelease(6)){	
 			active = !active;
 		}
+		if (active){
+			maxSpeed();
+		}else{
+			stop();
+		}
+	
 	}
 	
 	
